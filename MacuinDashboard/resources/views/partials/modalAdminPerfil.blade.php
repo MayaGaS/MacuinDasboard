279c8b8995642e0b33c5bf2c94ebc4f94357fc3f
MayaGaS <11mayagarrido.11@gmail.com>
2024-03-12 16:25:39 -0600

    navbar, modals

--- MacuinDashboard/resources/views/partials/modalAdminPerfil.blade.php
+++ MacuinDashboard/resources/views/partials/modalAdminPerfil.blade.php
@@ -1,18 +1,18 @@
-<div class="fixed top-10 right-3 mt-8">
-    <div class="bg-purple-200 p-6 rounded-lg shadow-lg max-w-md w-full md:w-70">
-        <div class="flex items-start space-x-2 mb-4">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="h-6 w-6 text-purple-500">
-                <!-- Aquí debes reemplazar con el contenido SVG real para tu icono -->
-            </svg>
-            <h2 class="text-xl font-semibold text-purple-500">Administración de mi perfil</h2>
+    <div class="fixed top-10 right-3 mt-8">
+        <div class="bg-purple-200 p-6 rounded-lg shadow-lg max-w-md w-full md:w-70">
+            <div class="flex items-start space-x-2 mb-4">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="h-6 w-6 text-purple-500">
+                    <!-- Aquí debes reemplazar con el contenido SVG real para tu icono -->
+                </svg>
+                <h2 class="text-xl font-semibold text-purple-500">Administración de mi perfil</h2>
+            </div>
+            <ul class="text-center">
+                <li class="mb-2 text-center"><a href="#" class="text-gray-700 hover:text-purple-500">Editar mis datos personales</a></li>
+                <li class="mb-2 text-center"><a href="#" class="text-gray-700 hover:text-purple-500">Privacidad</a></li>
+                <li class="mb-2 text-center"><a href="#" class="text-gray-700 hover:text-purple-500">Configuración de mi cuenta</a></li>
+            </ul>
+            <button type="button" class="mx-auto flex items-center justify-center rounded-full border border-transparent shadow-sm px-4 py-2 bg-purple-600 text-sm font-medium text-white hover:bg-purple-700 focus:outline-none focus:ring-2 focus:border-purple-700 focus:ring-offset-2 focus:ring-offset-white">
+                Cerrar Sesión
+            </button>
         </div>
-        <ul class="text-center">
-            <li class="mb-2 text-center"><a href="#" class="text-gray-700 hover:text-purple-500">Editar mis datos personales</a></li>
-            <li class="mb-2 text-center"><a href="#" class="text-gray-700 hover:text-purple-500">Privacidad</a></li>
-            <li class="mb-2 text-center"><a href="#" class="text-gray-700 hover:text-purple-500">Configuración de mi cuenta</a></li>
-        </ul>
-        <button type="button" class="mx-auto flex items-center justify-center rounded-full border border-transparent shadow-sm px-4 py-1 bg-purple-600 text-sm font-medium text-white hover:bg-purple-700 focus:outline-none focus:ring-2 focus:border-purple-700 focus:ring-offset-2 focus:ring-offset-white">
-            Cerrar Sesión
-        </button>
     </div>
-</div>
--- MacuinDashboard/resources/views/partials/navbar.blade.php
+++ MacuinDashboard/resources/views/partials/navbar.blade.php
@@ -114,33 +114,6 @@
             aria-orientation="vertical"
             aria-labelledby="options-menu"
         >
-            <div class="py-1" role="none">
-            <a
-                href="#"
-                class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900"
-                role="menuitem"
-                >Account settings</a
-            >
-            <a
-                href="#"
-                class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900"
-                role="menuitem"
-                >Support</a
-            >
-            <a
-                href="#"
-                class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900"
-                role="menuitem"
-                >License</a
-            >
-            <form method="POST" action="#" role="none">
-                <button
-                type="submit"
-                class="block w-full text-left px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900"
-                role="menuitem"
-                >
-                Sign out
-                </button>
             </form>
             </div>
         </div>
