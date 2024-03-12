23253b70f660ffa102c503a871570e86edbb9fe1
monserratvargass <guvamonse@gmail.com>
2024-03-11 23:55:37 -0600

    vista consultar y eliminar ticket del modulo cliente

---
+++ MacuinDashboard/resources/views/cliente/eliminarTicket.blade.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
+  <title>Cancelar Tickets</title>
+</head>
+<body class="bg-gray-200">
+    @include('partials/navbar')
+    <div class="flex justify-end">
+        <form class="bg-gray-100 p-6 rounded-lg w-96">
+          <h2 class="text-xl mb-4">Cancelar Tickets</h2>
+          
+          <div class="mb-4">
+            <label for="ticketNumber" class="sr-only">No.Ticket</label>
+            <input type="text" id="ticketNumber" name="ticketNumber" placeholder="No.Ticket" 
+                   class="bg-white border rounded-lg w-full p-2">
+          </div>
+      
+          <div class="mb-4">
+            <label for="date" class="sr-only">Fecha</label>
+            <input type="date" id ="date" name ="date"
+                   class ="bg-white border rounded-lg w-full p-2">
+          </div>
+      
+          <!-- Agrega más campos de entrada según sea necesario -->
+      
+          <button type ="submit"
+                  class ="bg-purple-500 text-white rounded-lg px-4 py-2 mt -4 hover:bg-red -600 focus:outline-none focus:border-red -700 focus:ring focus:ring-red -200 active:bg-red -700"
+                  >Cancelar</button>
+        </form>
+      </div>
+    </body>
+    </html>   
